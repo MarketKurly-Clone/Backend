b66f5bdfed63272c03324e87e68ab831281084d8
namelim <lim49435150@gmail.com>
2021-10-18 15:47:31 +0900

    main 위치 맞추기

---
+++ src/test/java/com/sparta/kerly_clone/KerlyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kerly_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerlyCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
